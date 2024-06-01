ff5dc7b757b198c80d778f56581ccf3af6add815
Samuel Ricardo Quintero Agudelo <squinter@bancolombia.com.co>
2024-06-01 10:55:11 -0500

    Propagate exception

--- src/main/java/com/ias/inventory/application/ports/inputs/IProductServicePort.java
+++ src/main/java/com/ias/inventory/application/ports/inputs/IProductServicePort.java
@@ -1,13 +1,14 @@
 package com.ias.inventory.application.ports.inputs;
 
+import com.ias.inventory.domain.exception.ProductNotFoundException;
 import com.ias.inventory.domain.models.Product;
 
 import java.util.List;
 
 public interface IProductServicePort {
-    Product findProductById(Long id);
+    Product findProductById(Long id) throws ProductNotFoundException;
     List<Product> findAllProducts();
     Product saveProduct(Product product);
-    Product updateProduct(Long id, Product product);
-    void deleteById(Long id);
+    Product updateProduct(Long id, Product product) throws ProductNotFoundException;
+    void deleteById(Long id) throws ProductNotFoundException;
 }
--- src/main/java/com/ias/inventory/application/ports/outputs/IProductPersistencePort.java
+++ src/main/java/com/ias/inventory/application/ports/outputs/IProductPersistencePort.java
@@ -1,10 +1,12 @@
 package com.ias.inventory.application.ports.outputs;
 
 import com.ias.inventory.domain.models.Product;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public interface IProductPersistencePort {
     Optional<Product> findById(Long id);
     List<Product> findAll();
